af89b217db32c1efaf976c2f86a9b42cc134b416
Isaac Cirlin <isaaccirlin@gmail.com>
2023-03-12 14:11:47 -0500

    Added the ability to place numbers on the board, an algorithm for checking the game state and a couple of exceptions for transmitting the game state

--- RandomNumberGame.py
+++ RandomNumberGame.py
@@ -1,14 +1,26 @@
 # OK, WE'RE MAKING A GAME
 #
 import array
+import dataclasses
 from collections import namedtuple
 import random
 
 import art
 import colorama
+import itertools
+from enum import Enum
 
 MenuOption = namedtuple("Option", "text, callback")
 Menu = namedtuple("menu", "title, menu_options")
+
+
+class GameStateException(Exception):
+    """Raised when an action would cause an invalid game state"""
+    pass
+
+class GameOver(Exception):
+    """ An easy way to pass a game over message to the game handler """
+    pass
 
 
 class Game:
@@ -25,41 +37,77 @@ class Game:
 
     _DEFAULT_VALUE = None
 
-    _range: (int, int)
-    _board: tuple
-    _board_size: int
-    _random_numbers: list[int]
+    __range: (int, int)
+    __board: tuple
+    __board_size: int
+    __random_numbers: list[int]
+
+    __current_number: int = _DEFAULT_VALUE
 
     def set_random_range(self, max_value: int):
         """ we're allowing for non-zero minimums. Dunno why """
-        self._range = (0, max_value)
+        self.__range = (0, max_value)
         return self
 
     def set_board_size(self, new_size: int):
         """ set the number of spaces in the board to place numbers """
-        self._board_size = new_size
+        self.__board_size = new_size
         return self
 
     def reset_game(self):
         """ reset the board using the current game configuration """
-        self._board = ([self._DEFAULT_VALUE] * self._board_size)
+        self.__board = ([self._DEFAULT_VALUE] * self.__board_size)
         self._generate_random_numbers()
+        return self
+
+    def _is_board_filled(self) -> bool:
+        return all(self.__board)
+
+    def _is_board_ordered(self) -> bool:
+        """ algorithm to validate that the board state is currently in order. the game ends when this returns false """
+        for first, second in pairwise(filter(lambda x: x is not None, self.__board)):
+            if first > second:
+                print("FAILURE")
+                return False
+        return True
+
+    def game_over(self) -> bool:
+        if not self._is_board_ordered():
+            raise GameOver(in_red("YOU HAVE FAILED, BETTER LUCK NEXT TIME"))
+        if self._is_board_filled():
+            raise GameOver()
+
+        return self._is_board_filled() or not self._is_board_ordered()
+
+    def place_current_number(self, index: int):
+        """ place the current number on the board and change the current to default to indicate it's processed """
+        self.__board[index] = self.__current_number
+        self.__current_number = self._DEFAULT_VALUE
         return self
 
     def print_board(self):
         """ print out a nice copy of the board to look at """
-        for index, value in enumerate(self._board):
-            print(f'{index}:{value if value else "___"} ', end=" ")
+        for index, value in enumerate(self.__board):
+            print(f'{in_magenta(str(index))}:{value or "___"} ', end=" ")
         print()  # add the newline
         return self
 
+    def prompt_next_move(self):
+        """ displays the current state and prompts the user for the next move """
+        self.print_board()
+        index = prompt(f'{in_red("PLACE YOUR NUMBER:")} {self.__current_number}')
+        self.place_current_number(index)
+
     def get_random_number(self):
-        """ returns the next random number from the pre-generated list of randoms """
-        return self._random_numbers.pop()
+        """ load the next pre-generated random number and return the value """
+        if self.__current_number:
+            raise GameStateException("Current number must be consumed before the next can be provided")
+        self.__current_number = self.__random_numbers.pop()
+        return self.__current_number
 
     def cheat(self):
         """ you should feel ashamed of yourself """
-        print(self._random_numbers)
+        print(self.__random_numbers)
         return self
 
     def _generate_random_numbers(self):
@@ -78,12 +126,29 @@ class Game:
 
                     Learn More: https://www.tutorialspoint.com/functional_programming/functional_programming_lazy_evaluation.htm
         """
-        self._random_numbers = random.sample(range(*self._range), self._board_size)
+        self.__random_numbers = random.sample(range(*self.__range), self.__board_size)
 
+
+def pairwise(values: iter):
+    """
+    generator function to return consecutive pairs from an iterator [ABCDE] -> AB, BC, CD, DE
+    Learn More: https://www.geeksforgeeks.org/generators-in-python/
+    """
+    current = None
+    for index in values:
+        last = current
+        current = index
+        if last is not None and current is not None:
+            yield last, current
 
 def in_red(text):
     """ Use a cool library I found to turn some text red """
-    return colorama.Fore.RED + text
+    return colorama.Fore.RED + text + colorama.Fore.RESET
+
+
+def in_magenta(text):
+    """ Use a cool library I found to turn some text magenta """
+    return colorama.Fore.MAGENTA + text + colorama.Fore.RESET
 
 
 def prompt(message: str = in_red("YOU MUST CHOOSE"), default: int = None) -> int:
@@ -99,14 +164,24 @@ def print_menu(menu_options: list[MenuOption]):
     menu_options[prompt() - 1].callback()
 
 
+def run_game(game: Game):
+    game.reset_game()
+    while not game.game_over():
+        next_number = game.get_random_number()
+        game.print_board()
+        index = prompt(f'{in_red("PLACE YOUR NUMBER:")} {next_number}')
+        game.place_current_number(index)
+    print("GAME OVER")
+    print_main_menu()
+
+
 def start_new_game():
     """ prompt the user for game configurations and kick off a new round """
     game = Game() \
         .set_random_range(prompt(message="Pick a really big number like", default=1000)) \
         .set_board_size(prompt(message="Now pick a really small number", default=10)) \
-        .reset_game() \
-        .print_board() \
-        .cheat()
+        .reset_game()
+    run_game(game)
 
 
 def print_main_menu():
