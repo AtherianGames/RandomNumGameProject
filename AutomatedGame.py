f5e639c64c7ff57aaf002d4bd70a842b15477afe
ejross42 <ejross42@tntech.edu>
2023-03-23 21:10:44 -0500

    Implemented the Random Algorithm Method

--- AutomatedGame.py
+++ AutomatedGame.py
@@ -1,6 +1,7 @@
 # import random
+import random
 
-from RandomNumberGame import start_new_game, print_menu, prompt
+from RandomNumberGame import start_new_game, print_menu, prompt, Outcome
 import RandomNumberGame as rng
 import art
 from collections import namedtuple
@@ -15,7 +16,7 @@ class Automator:
     attempts = []
 
 def run_games(auto, game):
-    for attempt in range(0, auto.total_num_runs):
+    for attempt_num in range(0, auto.total_num_runs):
         game.reset_game()
         index = 0
         while True:
@@ -30,11 +31,48 @@ def run_games(auto, game):
                 auto.attempts.append(e.outcome)
                 break
             except rng.GameStateException as e:
-                print(index, attempt)
+                print(index, attempt_num)
                 game.print_board()
                 print(rng.in_red(f'\nGameStateException: {e}'))
                 break
-    print(auto.attempts)
+    #print(auto.attempts)
+    successful_attempts = sum([1 for result in auto.attempts if result is Outcome.WIN])
+    failed_attempts = sum([1 for result in auto.attempts if result is Outcome.LOSE])
+    percentage_successful: float = successful_attempts/auto.total_num_runs
+    print(f"Successful Attempts: {successful_attempts} , Failed Attempts: {failed_attempts} (Percentage: {percentage_successful}%)")
+    print()
+    print_main_menu()
+
+def run_random_games(auto, game):
+    for attempt_num in range(0, auto.total_num_runs):
+        game.reset_game()
+        index = 0
+        spaces = [*range(0, auto.total_num_runs)]
+        random.shuffle(spaces)
+        while True:
+            try:
+                next_number = game.get_random_number()
+                space = spaces.pop()
+                # game.print_board()
+                # index = prompt(f'{in_blue("PLACE YOUR NUMBER:")} {next_number}')
+                print(f'Placing value: {next_number} at index: {space}')
+                game.place_current_number(space)
+
+            except rng.GameOver as e:
+                auto.attempts.append(e.outcome)
+                game.print_board()
+                break
+            except rng.GameStateException as e:
+                print(index, attempt_num)
+                game.print_board()
+                print(rng.in_red(f'\nGameStateException: {e}'))
+                break
+    #print(auto.attempts)
+    successful_attempts = sum([1 for result in auto.attempts if result is Outcome.WIN])
+    failed_attempts = sum([1 for result in auto.attempts if result is Outcome.LOSE])
+    percentage_successful: float = successful_attempts/auto.total_num_runs
+    print(f"Successful Attempts: {successful_attempts} , Failed Attempts: {failed_attempts} (Percentage: {percentage_successful}%)")
+    print()
     print_main_menu()
 
 def print_main_menu():
@@ -55,7 +93,7 @@ def automate_game():
     game.board_size = prompt(message="Now pick a really small number", default=10)
     auto = Automator()
     auto.total_num_runs = prompt(message="Choose how many games you would like to automate", default=10)
-    run_games(auto, game)
+    run_random_games(auto, game)
 
 if __name__ == '__main__':
     # __init__()
